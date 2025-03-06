dd22bd57d0641306417cfe10c4bc5cc5ff663fda
Jesus Bejar <israelbejarii@gmail.com>
2025-03-06 08:37:53 -0700

    made comments

---
+++ src/ts/income.ts
@@ -0,0 +1,37 @@
+// define Income type
+type Income = {
+    amount: number;
+    description: string;
+    date: string;
+}
+
+// getIncome(), adds income amount to a list, void
+
+    // define incomeList
+
+    // push income to incomeList then to localStorage
+
+    // call displayIncome();
+
+
+// displayIncome(), displays the income objects in a list, void
+
+    // grab income objects from localStorage
+
+    // define income objects into HTML var
+
+    // inject HTML var into actual HTML
+
+// fireIncomeTracker(), gets all income object input data, fires displayIncome(), void
+
+    // call displayIncome();
+
+    // get form element
+
+    // define form event listener, e.preventDefault();
+
+    // get amount and description elements
+
+    // call addIncome();, define date var
+
+    // clear form
