6bca126795107c0780d2742d500404581c40f99f
Jesus Bejar <israelbejarii@gmail.com>
2025-03-08 08:02:24 -0600

    made comments for expenses.ts

---
+++ src/ts/expenses.ts
@@ -0,0 +1,47 @@
+// define expense type
+type Expense = {
+    amount: number, 
+    category: string, 
+    recurring: boolean, 
+    date: string
+}
+
+// getExpense(), adds expense amount to a list, void
+ 
+     // define expensesList
+ 
+     // push expense to incomesList then to localStorage
+ 
+     // call displayExpenses();
+     // call checkRecurring();
+
+// displayExpenses(), displays the expense objects in a list, void
+ 
+     // grab expense objects from localStorage
+ 
+     // define expense objects into HTML var
+ 
+     // inject HTML var into actual HTML
+
+// checkRecurring(), displays possible recurring costs in "#recurring-alerts", void
+
+    // load expenses in localStorage
+
+    // filter through expenses
+
+    // inject HTML var(a str detailing recurring expenses) into actual HTML
+
+// fireExpenseTracker(), gets all expense object input data, fires displayExpense(), void
+ 
+     // call displayExpense();
+     // call checkRecurring();
+ 
+     // get form element
+ 
+     // define form event listener, e.preventDefault();
+ 
+     // get amount, category and recurring elements
+ 
+     // call addExpense();, define date var
+ 
+     // clear form
