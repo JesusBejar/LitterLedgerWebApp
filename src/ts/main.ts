7f54d26ebb4e541b187012113b4653e3c59aeeb2
Jesus Bejar <israelbejarii@gmail.com>
2025-03-08 09:17:19 -0600

    tested fireExpenseTracker()

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,20 +1,20 @@
 // import functions
 import { fireIncomeTracker } from "./income"
-import { getExpenses, displayExpenses, checkRecurring } from './expenses';
+import { fireExpenseTracker } from './expenses';
 
 // income.ts testing calls below - 
 // console.log("Testing getIncome():", getIncome({amount: 10, description: "hola", date: "2025"})); // Should print income data in the console
 // (window as any).displayIncome = displayIncome;
 
 // expenses.ts testing calls below - 
-console.log("Testing getExpenses():", getExpenses({amount: 10, category: "yo momma", recurring: true, date: "2025"})); // Should print expense data in the console
-(window as any).displayExpenses = displayExpenses;
-(window as any).checkRecurring = checkRecurring;
+// console.log("Testing getExpenses():", getExpenses({amount: 10, category: "yo momma", recurring: true, date: "2025"})); // Should print expense data in the console
+// (window as any).displayExpenses = displayExpenses;
+// (window as any).checkRecurring = checkRecurring;
 
 // where all functions are fire
 document.addEventListener("DOMContentLoaded", () => {
     fireIncomeTracker();
-    // fireExpenseTracker();
+    fireExpenseTracker();
     // fireProfitCalculator();
     // document.querySelector("export-btn")!.addEventListener("click", exportTaxRecords());
 })
