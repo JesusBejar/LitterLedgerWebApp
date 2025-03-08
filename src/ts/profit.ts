49f6a2370e6b6ff937e63f4e91dd6fd0a3115a79
Jesus Bejar <israelbejarii@gmail.com>
2025-03-08 11:19:36 -0600

    made comments for profits.ts

---
+++ src/ts/profit.ts
@@ -0,0 +1,31 @@
+type Income= {
+    amount: number;
+    description: string;
+    date: string;
+}
+
+type Expense = {
+    amount: number;
+    category: string;
+    recurring: boolean;
+    date: string;
+}
+// calculateProfit(), calculates profits with income and expense objects, number
+
+    // define incomeList
+
+    // define expenseList
+
+    // reduce income object amounts to single value
+
+    // reduce expense object amounts to single value
+
+    // return result
+
+// fireProfitCalculator, displays total profit, void
+
+    // call calculateProfit(), assign to var
+
+    // inject into HTMl using html var
+
+
