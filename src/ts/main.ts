04ad70abd912fef9804e3500e8c50c3dc8409d0d
Jesus Bejar <israelbejarii@gmail.com>
2025-03-08 09:05:02 -0600

    tested checkRecurring()

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,6 @@
 // import functions
 import { fireIncomeTracker } from "./income"
-import { getExpenses, displayExpenses } from './expenses';
+import { getExpenses, displayExpenses, checkRecurring } from './expenses';
 
 // income.ts testing calls below - 
 // console.log("Testing getIncome():", getIncome({amount: 10, description: "hola", date: "2025"})); // Should print income data in the console
@@ -9,7 +9,7 @@ import { getExpenses, displayExpenses } from './expenses';
 // expenses.ts testing calls below - 
 console.log("Testing getExpenses():", getExpenses({amount: 10, category: "yo momma", recurring: true, date: "2025"})); // Should print expense data in the console
 (window as any).displayExpenses = displayExpenses;
-
+(window as any).checkRecurring = checkRecurring;
 
 // where all functions are fire
 document.addEventListener("DOMContentLoaded", () => {
