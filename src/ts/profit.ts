683839ed014c5025e6374115063ad106a4148f17
Jesus Bejar <israelbejarii@gmail.com>
2025-03-10 08:08:50 -0500

    made calculateProfit()

--- src/ts/profit.ts
+++ src/ts/profit.ts
@@ -11,16 +11,22 @@ type Expense = {
     date: string;
 }
 // calculateProfit(), calculates profits with income and expense objects, number
-
+export function calculateProfit(): number {
     // define incomeList
-
+    const income: Income[] = JSON.parse(localStorage.getItem("income") || "[]")
     // define expenseList
+    const expenses: Expense[] = JSON.parse(localStorage.getItem("expense") || "[]")
 
     // reduce income object amounts to single value
-
+    const totalIncome = income.reduce((sum, i ) => sum + i.amount, 0)
+    // console.log(totalIncome)
     // reduce expense object amounts to single value
-
+    const totalExpenses = expenses.reduce((sum, i ) => sum + i.amount, 0)
+    // console.log(totalExpenses)
+    
     // return result
+    return totalExpenses - totalIncome
+}
 
 // fireProfitCalculator, displays total profit, void
 
